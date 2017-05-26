17fed31a311b372ff92d7550c72b0b5c28f62f8f
DESKTOP-KUB296A\locus <locus84@gmail.com>
2017-05-26 12:33:23 +0900

    adding test for messagefiber

---
+++ Locus.ThreadingTest/MessageFiberTest.cs
@@ -0,0 +1,43 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Locus.Threading;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace Locus.ThreadingTest
+{
+    [TestClass]
+    public class MessageFiberTest
+    {
+        public class TestMsgFiber : MessageFiber<int>
+        {
+            public int i = 0;
+            public List<Exception> exceptions = new List<Exception>();
+            protected override void OnException(Exception exception)
+            {
+                exceptions.Add(exception);
+            }
+
+            protected override void OnMessage(int message)
+            {
+                i += message;
+            }
+        }
+
+        [TestMethod]
+        public void TestFunc()
+        {
+            //this test is to examine the actions are executed in thread safe manner in threadpool
+            var tmf = new TestMsgFiber();
+            var add = MultiThreadTest.RunMultiple(() => tmf.Enqueue(1), 10000);
+            var minus = MultiThreadTest.RunMultiple(() => tmf.Enqueue(-1), 10000);
+            System.Threading.Tasks.Task.WhenAll(add).Wait();
+            System.Threading.Tasks.Task.WhenAll(minus).Wait();
+            //the i variable must be 0, and exception count should be zero too
+            Assert.IsTrue(tmf.i == 0);
+            Assert.IsTrue(tmf.exceptions.Count == 0);
+        }
+    }
+}
---
+++ Locus.ThreadingTest/MultiThreadTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locus.ThreadingTest
+{
+    //this is helper class to simplify test of multithreading environment
+    //here we make use of threading.task so we can rely on it's functionality
+    public class MultiThreadTest
+    {
+        public static IEnumerable<Task> RunMultiple(Action toRun, int count)
+        {
+            var taskList = new List<Task>();
+            for (int i = 0; i < count; i++)
+                taskList.Add(Task.Run(toRun));
+            return taskList;
+        }
+    }
+}
